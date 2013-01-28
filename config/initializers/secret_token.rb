74e99c3b5ee370e946386e69e0c7916e011d1397
Hooly-Gun <Fasper.dev@yandex.ru>
2013-01-28 16:52:48 +0400

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fasper::Application.config.secret_token = 'ef2bce764832affc9ab95b4c7692bc2c8843acc3cdf61745e847942934e58a0f466269e9930eb6b108297dbaef015541e6bf85cb84defe14e212fdd92a71a0ee'
